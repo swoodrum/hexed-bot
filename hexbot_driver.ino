c86d137e50f89c44c8326547bebee6a97c37fc3c
Scott Woodrum <swoodrum@gmail.com>
2012-06-03 17:31:44 -0500

    hacking on light tracking

--- hexbot_driver.ino
+++ hexbot_driver.ino
@@ -1,4 +1,8 @@
 #include <SoftwareSerial.h>
+#include <TVout.h>
+#include <fontALL.h>
+#define W 128
+#define H 96
 
 // NOTE: digital pins 2, 6, 7, 8, and optionally 9 are used by the video shield.
 // the video shield also uses analog pin 2
@@ -9,7 +13,7 @@
 #define DEFAULT_BAUD 9600
 #define DEVICE_NUM 12 // default device number for Pololu protocol
 #define MIN_RANGE 30
-#define PROTOCOL_BYTE 170 // Pololu protocol identifier
+#define PROTOCOL_BYTE 170 // Pololu protocol identifier (0xAA)
 #define SERVO_0_NEUTRAL 4632
 #define SERVO_0_MAX 6360
 #define SERVO_0_MIN 2752
@@ -22,26 +26,129 @@
 
 const int gp2d12Pin = 5; // analog pin 5
 
+TVout tv;
+
 SoftwareSerial mySerial(pRxPin, pTxPin);
 
+unsigned char c;
+unsigned char minX,minY,maxX,maxY;
+char s[32];
+
+int servoX = SERVO_1_NEUTRAL;
+
 void setup() {
+  tv.begin(NTSC, W, H);
+  initOverlay();
+  initInputProcessing();
   Serial.begin(DEFAULT_BAUD);
   mySerial.begin(DEFAULT_BAUD);
   delay(1000);
   center_servos();
+  
 }
 
 void loop() {
   //Serial.println(read_gp2d12_range(gp2d12Pin));
-  if(read_gp2d12_range(gp2d12Pin) > MIN_RANGE) {
-    walk_forward(1); 
-  } 
-  else {
-    walk_backward(1); 
+  /*if(read_gp2d12_range(gp2d12Pin) > MIN_RANGE) {
+   walk_forward(1); 
+   } 
+   else {
+   walk_backward(1); 
+   }*/
+  //sweep();
+  //pan();
+  tv.capture();
+  minX = W;
+  minY = H;
+  maxX = 0;
+  maxY = 0;
+  boolean found = 0;
+  for(int y=0;y<H;y++) {
+    for(int x=0;x<W;x++) {
+      c = tv.get_pixel(x,y);
+      if (c == 1) {
+        found = true;
+        if (x < minX) {
+          minX = x;
+        }
+        if (x > maxX) {
+          maxX = x;
+        }
+        if (y < minY) {
+          minY = y;
+        }
+        if (y > maxY) {
+          maxY = y;
+        }
+      }
+    }
   }
-  sweep();
-  pan();
-  delay(5000);
+  if(found) {
+    /*Serial.print("minX: ");
+     Serial.print(minX);
+     Serial.print("\tmaxX: ");
+     Serial.print(maxX);
+     Serial.print("\tminY: ");
+     Serial.print(minY);
+     Serial.print("\tmaxY: ");
+     Serial.println(maxY);*/
+    int rawX = (maxX+minX)/2;
+    //int translateX = map(rawX, 0, W, SERVO_1_MAX, SERVO_1_MIN);
+    int translateX = map(rawX, 0, W, SERVO_1_MIN, SERVO_1_MAX);
+    
+    //int rawY = (maxY+minY)/2;
+    //int translateY = map(rawY, 0, H, SERVO_0_MIN, SERVO_0_MAX);
+    //sprintf(s, "%d, %d", ((maxX+minX)/2), ((maxY+minY)/2));
+    //sprintf(s, "%d, %d", rawX, rawY);
+    //Serial.println(translateX);
+    if(translateX > 6000) {
+      servoX += (translateX - servoX);
+    } else if (translateX < 6000) {
+      servoX -= (servoX - translateX); 
+    }
+    //Serial.println(servoX);
+    //Serial.print(", ");
+    //Serial.println(translateY);
+    if((servoX <= SERVO_1_MAX) && (servoX >= SERVO_1_MIN)) {
+      move_servo(1, servoX); 
+    }
+    //move_servo(0, translateY);
+
+    //move_servo(1, constrain(servoX, SERVO_1_MIN, SERVO_1_MAX));
+    //Serial.println(servoX);
+  }
+  delay(1000);
+  //tv.resume();
+}
+
+// misc tv functions
+void initOverlay() {
+  TCCR1A = 0;
+  // Enable timer1.  ICES0 is set to 0 for falling edge detection on input capture pin.
+  TCCR1B = _BV(CS10);
+
+  // Enable input capture interrupt
+  TIMSK1 |= _BV(ICIE1);
+
+  // Enable external interrupt INT0 on pin 2 with falling edge.
+  EIMSK = _BV(INT0);
+  EICRA = _BV(ISC11);
+}
+
+void initInputProcessing() {
+  // Analog Comparator setup
+  ADCSRA &= ~_BV(ADEN); // disable ADC
+  ADCSRB |= _BV(ACME); // enable ADC multiplexer
+  ADMUX &= ~_BV(MUX0);  // select A2 for use as AIN1 (negative voltage of comparator)
+  ADMUX |= _BV(MUX1);
+  ADMUX &= ~_BV(MUX2);
+  ACSR &= ~_BV(ACIE);  // disable analog comparator interrupts
+  ACSR &= ~_BV(ACIC);  // disable analog comparator input capture
+}
+
+// Required
+ISR(INT0_vect) {
+  display.scanLine = 0;
 }
 
 /* 
@@ -67,7 +174,7 @@ float read_gp2d12_range(byte pin) {
 
 void move_servo(int channel, int pos) {
   byte command[] = {
-    PROTOCOL_BYTE, DEVICE_NUM, SET_TARGET_BYTE, channel, pos & 0x7F, (pos >> 7) & 0x7F };
+    PROTOCOL_BYTE, DEVICE_NUM, SET_TARGET_BYTE, channel, pos & 0x7F, (pos >> 7) & 0x7F                 };
   for(int i = 0; i < 6; i++) {
     mySerial.write(command[i]);
     //Serial.println(command[i],HEX);
@@ -221,6 +328,14 @@ void turn_left(int numTurns) {
   }
   center_servos();
 }
+
+
+
+
+
+
+
+
 
 
 
