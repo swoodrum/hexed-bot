3f4f66df879fb5dfd54b8af97307858211b7ee65
Scott Woodrum <Scott's Dell i5@MININT-HGRU73E.(none)>
2012-06-16 18:54:04 -0500

    code cleanup
    Signed-off-by: Scott Woodrum <Scott's Dell i5@MININT-HGRU73E.(none)>

--- hexbot_driver/hexbot_driver.ino
+++ hexbot_driver/hexbot_driver.ino
@@ -2,14 +2,6 @@
 #include <TVout.h>
 #include <fontALL.h>
 
-// bit twiddling macros
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif
-
 #define W 128
 #define H 96
 
@@ -152,7 +144,7 @@ void detect_light() {
 
 // misc tv functions
 void initOverlay() {
-  TCCR1A = 0;
+  TCCR1A = 0; // disable PWM on timer 1 during setup
   // Enable timer1.  ICES0 is set to 0 for falling edge detection on input capture pin.
   TCCR1B = _BV(CS10);
 
@@ -165,7 +157,6 @@ void initOverlay() {
 }
 
 void initVideoProcessing() {
-  //TIMSK1 |= _BV(ICIE1);
   // Analog Comparator setup
   ADCSRA &= ~_BV(ADEN); // disable ADC
   ADCSRB |= _BV(ACME); // enable ADC multiplexer
@@ -174,20 +165,6 @@ void initVideoProcessing() {
   ADMUX &= ~_BV(MUX2);
   ACSR &= ~_BV(ACIE);  // disable analog comparator interrupts
   ACSR &= ~_BV(ACIC);  // disable analog comparator input capture
-  //delay(10);
-}
-
-void disableVideoProcessing() {
-  TIMSK1 &= ~_BV(ICIE1);
-  // Analog Comparator setup
-  ADCSRA |= _BV(ADEN); // disable ADC
-  ADCSRB &= ~_BV(ACME); // enable ADC multiplexer
-  ADMUX |= _BV(MUX0);  // select A2 for use as AIN1 (negative voltage of comparator)
-  ADMUX &= ~_BV(MUX1);
-  ADMUX |= _BV(MUX2);
-  ACSR |= _BV(ACIE);  // disable analog comparator interrupts
-  ACSR |= _BV(ACIC);
-  //delay(10);
 }
 
 // Required
